8e9dc65471177705980629a09987ab9998fe09c9
maymikb <123998301+maymikb@users.noreply.github.com>
2024-02-16 09:27:03 -0800

    setup,questions

---
+++ javascript.js
@@ -0,0 +1,79 @@
+const questions=[
+    {
+        question:"Quotation marks enclose ",
+        choices: [
+            "strings",
+            "objects",
+            "numbers",
+            "arrays",
+            
+        ], correctAnswerIndex:0,
+    }
+
+    {
+        question:"What does HTML stand for?",
+        choices: [
+            "Helpful Text Maker Language",
+            "Hyper Text Markup Language",
+            "Hotmail",
+            "Hotmail Type Markup Language",
+            
+        ], correctAnswerIndex:1,
+    }
+
+    {
+        question:"An array is enclosed in _?",
+        choices: [
+            "curly brackets",
+            "square brackets",
+            "parentheses",
+            "quotes",
+            
+        ], correctAnswerIndex: 1,
+    }
+
+    {
+        question:"What static command prints a message to the console?",
+        choices: [
+            "print solution",
+            "preview solution",
+            "console.log()",
+            "console.preview",
+            
+        ], correctAnswerIndex:2,
+    }
+
+    {
+        question:"How do you start an HTML document?",
+        choices: [
+            "<HTML>",
+            "title",
+            "<!DOCTYPE html>",
+            "introdution",
+            
+        ], correctAnswerIndex: ,
+    }
+
+    {
+        question:"What does CSS stand for?",
+        choices: [
+            "cascading style sheets",
+            "html styling",
+            "core styling sheet",
+            "code style supply",
+            
+        ], correctAnswerIndex:0,
+    }
+
+    {
+        question:"Curly braces enclose which type(s) of data?",
+        choices: [
+            "arrays",
+            "null values",
+            "booleans",
+            "objects",
+            
+        ], correctAnswerIndex: 3,
+    }
+]
+
